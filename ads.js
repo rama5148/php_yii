7a48975041793044373816a9a2e5368d0dc6d9f8
rama5148 <30627560+rama5148@users.noreply.github.com>
2020-05-15 11:11:21 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,7 @@
+// tes view image
+
+function load(){
+	var url= "https://cdn.pixabay.com/photo/2016/11/27/08/24/internet-1862312_960_720.jpg";
+		document.getElementById('page').innerHTML="<img src='"+ url +"' width='600' height='100%'/>"
+}
+load();
